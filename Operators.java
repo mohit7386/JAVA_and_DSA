01f172a220b730b88b1936cf58d4b7644ef765c3
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2024-11-28 08:43:21 +0530

    New Changes Added in the repository

--- Operators.java
+++ Operators.java
@@ -57,11 +57,9 @@ public class Operators {
                                     // equivalent is 10 because output jo hai wo 10 dikhega kyuki binay format
                                     // convert hoke decimal ke equivalent show hota hai output mein
         int j = 7; // Binary of 7 is 0111 after left shift answer is - 1110
-        System.out.println(j << 1); // left shift by 1 and the decimal equivalent is 14 because decimal equivalent
-                                    // or integer me jab output aata hai to number ki multiply hoti hai 2 se like
-                                    // 7*2^1 = 14 is output or saath hi left shift jab karte hain to left me bits
-                                    // shift hota hai aur right me zeroes add hote hain jisse sahi se ho jaata hai
-                                    // binary ka shifting
+        System.out.println(j << 1); // Dekho kuch mat karo left shift dekhlo karke ki kitna aayega answer but jo
+                                    // output show hoga wo decimal equivalent show hoga means ki jo number hai na
+                                    // usko 2 se multiply kar do to answer aa jayega.
 
     }
 
