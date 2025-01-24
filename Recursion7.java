e2712feece0c8e3dde94f323ccabb8f01fc7ad2a
M_O_H_I_T3456 <61095721+mohit7386@users.noreply.github.com>
2025-01-24 11:28:56 +0530

    New Changes added in the file just checking the git in cursor

--- Recursion7.java
+++ Recursion7.java
@@ -1,27 +1,33 @@
+
 //WAP to print the disk of tower of hanoi
 import java.util.*;
-public class Recursion7 {
-    static void towerOfHanoi(int N , String Src , String Hel , String Des){
-        //If only one disk , move it directly
-      if(N==1){ //If only one disk then move it directly from sorce se destination tak pahucha do  
-        System.out.println("Move disk "+N+ " from " +Src+ " to "+Des);
-        return;
-      }
-      //move (n-1) disk from source to destination and helper ban jaayega destination and destination ban jaayega helper
-      towerOfHanoi(N-1, Src, Des, Hel); 
-      //Move nth disk from the source to destination 
-      System.out.println("Move disk "+N+ " from " +Src+ " to "+Des);
-      //move n-1 disk from source to destination using source as a helper
-      towerOfHanoi(N-1, Hel, Src, Des);
 
+public class Recursion7 {
+  static void towerOfHanoi(int N, String Src, String Hel, String Des) {
+    // If only one disk , move it directly
+    if (N == 1) { // If only one disk then move it directly from sorce se destination tak pahucha
+                  // do
+      System.out.println("Move disk " + N + " from " + Src + " to " + Des);
+      return;
     }
-    public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the no. of disks!");
-        int N=sc.nextInt();
-        System.out.println("***********TOWER OF HANOI*************");
-        towerOfHanoi(N, "S", "H", "D");
-sc.close();
-    }
-    
+    // move (n-1) disk from source to destination and helper ban jaayega destination
+    // and destination ban jaayega helper
+    towerOfHanoi(N - 1, Src, Des, Hel);
+    // Move nth disk from the source to destination
+    System.out.println("Move disk " + N + " from " + Src + " to " + Des);
+    // move n-1 disk from source to destination using source as a helper
+    towerOfHanoi(N - 1, Hel, Src, Des);
+
+  }
+
+  // Now Writing the main function
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the no. of disks!");
+    int N = sc.nextInt();
+    System.out.println("***********TOWER OF HANOI*************");
+    towerOfHanoi(N, "S", "H", "D");
+    sc.close();
+  }
+
 }
